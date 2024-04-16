f74da803cd87545cb20bf16b6314dfcc3413c145
Szaki <49611651+Szaki-dev@users.noreply.github.com>
2024-04-16 18:18:47 +0200

    Update base.py

--- custom_components/tuya_fix/base.py
+++ custom_components/tuya_fix/base.py
@@ -43,21 +43,21 @@ class IntegerTypeData:
     @property
     def step_scaled(self) -> float:
         """Return the step scaled."""
-        return self.step / 1
+        return self.step / (10**1)
 
-    def scale_value(self, value: float | int) -> float:
+    def scale_value(self, value: float) -> float:
         """Scale a value."""
-        return value / 1
+        return value / (10**1)
 
-    def scale_value_back(self, value: float | int) -> int:
+    def scale_value_back(self, value: float) -> int:
         """Return raw value for scaled."""
-        return int(value * 1)
+        return int(value * (10**1))
 
     def remap_value_to(
         self,
         value: float,
-        to_min: float | int = 0,
-        to_max: float | int = 255,
+        to_min: float = 0,
+        to_max: float = 255,
         reverse: bool = False,
     ) -> float:
         """Remap a value from this range to a new range."""
@@ -66,8 +66,8 @@ class IntegerTypeData:
     def remap_value_from(
         self,
         value: float,
-        from_min: float | int = 0,
-        from_max: float | int = 255,
+        from_min: float = 0,
+        from_max: float = 255,
         reverse: bool = False,
     ) -> float:
         """Remap a value from its current range to this range."""
